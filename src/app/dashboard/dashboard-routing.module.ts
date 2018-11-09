3e42defb3b6647de04faa61def2e53633076cad1
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-11-09 01:01:32 -0300

    ajuste na rota do historico no modulo

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '', component: DashboardComponent, children: [
       { path: 'prontuarios', loadChildren: './prontuarios/prontuarios.module#ProntuariosModule' },
       { path: 'aprazamentos', loadChildren: './aprazamentos/aprazamentos.module#AprazamentosModule' },
-      { path: 'historico', loadChildren: './aprazamentos/historico.module#HistoricoModule' },
+      { path: 'historico', loadChildren: './historico/historico.module#HistoricoModule' },
       { path: '', redirectTo: 'prontuarios', pathMatch: 'full' }
     ]
   }
