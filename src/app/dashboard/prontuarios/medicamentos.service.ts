e0d0c0ddaeee67ef90fc48eaed407d32b60e4483
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-11-10 20:48:08 -0300

    Correcao do medicamento service

--- src/app/dashboard/prontuarios/medicamentos.service.ts
+++ src/app/dashboard/prontuarios/medicamentos.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
-import { Medicamento } from '../../models/medicamento.model';
+import { Medicamentos } from '../../models/medicamentos.model';
 import { ErrorHandler } from '../../app.error-handles';
 import { SALUS_API } from '../../app.api';
 
@@ -12,12 +12,12 @@ export class MedicamentosService {
 
   constructor(private http: HttpClient) { }
 
-  medicamentosById(ids: string[]): Observable<Medicamento> {
+  medicamentosById(ids: string[]): Observable<Medicamentos> {
     return new Observable((observer) => {
       ids.forEach(id => {
         this.http
         .get(`${SALUS_API}/medicamentos/${id}`).subscribe(
-          (medicamento: Medicamento) => observer.next(medicamento),
+          (medicamento: Medicamentos) => observer.next(medicamento),
           (error: any) => observer.error(error)
         );
       });
