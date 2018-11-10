21f64414e1324d26d18266f5e50d39b341217697
Luhan Lacerda <luhanlacerda@gmail.com>
2018-11-10 00:28:26 -0300

    Inicio dos ajustes na lista aprazamentos e testes de integracao com ws

--- src/app/dashboard/aprazamentos/listar-aprazamentos/listar-aprazamentos.component.ts
+++ src/app/dashboard/aprazamentos/listar-aprazamentos/listar-aprazamentos.component.ts
@@ -21,19 +21,19 @@ export class ListarAprazamentosComponent implements OnInit {
     .subscribe(aprazamentos => {
       this.aprazamentos = aprazamentos;
 
-      this.aprazamentos.push({ _id: '2322232', paciente: { _id: '329090', nome: '434332', sexo: 'M',
-      numeroRg: '', numeroCpf: '', nomeMae: '', nomePai: '', alergias: [] },
-      horario: new Date(2016, 9, 17, 20, 30, 0, 0),
-      enfermeira: { _id: '44343', name: '', email: '', cpf: '', accesToken: '' },
-      medicamento: { _id: '3232', nome: 'teste', dosagem: '3', dataFabricacao: '', dataValidade: new Date(2016, 9, 17, 20, 30, 0, 0) },
-      isConsumido: false, intervalo: '2', isCancelado: false });
+      // this.aprazamentos.push({ _id: '2322232', paciente: { _id: '329090', nome: '434332', sexo: 'M',
+      // numeroRg: '', numeroCpf: '', nomeMae: '', nomePai: '', alergias: [] },
+      // horario: new Date(2016, 9, 17, 20, 30, 0, 0),
+      // enfermeira: { _id: '44343', name: '', email: '', cpf: '', accesToken: '' },
+      // medicamento: { _id: '3232', nome: 'teste', dosagem: '3', dataFabricacao: '', dataValidade: new Date(2016, 9, 17, 20, 30, 0, 0) },
+      // isConsumido: false, intervalo: '2', isCancelado: false });
 
-      this.aprazamentos.push({ _id: '2322232', paciente: { _id: '329090', nome: '434332', sexo: 'M',
-      numeroRg: '', numeroCpf: '', nomeMae: '', nomePai: '', alergias: [] },
-      horario: new Date(2016, 9, 17, 20, 29, 0, 0),
-      enfermeira: { _id: '44343', name: '', email: '', cpf: '', accesToken: '' },
-      medicamento: { _id: '3232', nome: 'teste', dosagem: '3', dataFabricacao: '', dataValidade: new Date(2016, 9, 17, 20, 30, 0, 0) },
-      isConsumido: false, intervalo: '2', isCancelado: false });
+      // this.aprazamentos.push({ _id: '2322232', paciente: { _id: '329090', nome: '434332', sexo: 'M',
+      // numeroRg: '', numeroCpf: '', nomeMae: '', nomePai: '', alergias: [] },
+      // horario: new Date(2016, 9, 17, 20, 29, 0, 0),
+      // enfermeira: { _id: '44343', name: '', email: '', cpf: '', accesToken: '' },
+      // medicamento: { _id: '3232', nome: 'teste', dosagem: '3', dataFabricacao: '', dataValidade: new Date(2016, 9, 17, 20, 30, 0, 0) },
+      // isConsumido: false, intervalo: '2', isCancelado: false });
     });
   }
 
