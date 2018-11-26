8e8239d6d9f8c229a806c19cf37a38acf149e68a
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-11-25 21:44:34 -0300

    fix e refatoracao no componente de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,10 +7,10 @@ import { Usuario } from '../models/usuario.model';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    animations: [routerTransition()]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
 
@@ -20,44 +20,34 @@ export class LoginComponent implements OnInit {
   isInvalidLogin: boolean;
 
   constructor(public router: Router,
-              private fb: FormBuilder,
-              private loginService: LoginService) { }
+    private fb: FormBuilder,
+    private loginService: LoginService) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      username: this.fb.control('', [Validators.required, Validators.minLength(6)]), //Validators.pattern(/^[a-zA-Z]+$/)]),
+      username: this.fb.control('', [Validators.required, Validators.minLength(6)]),
       password: this.fb.control('', [Validators.required, Validators.minLength(6)])
     });
-    
-  //console.log("Limpando o LocalStorage..");
-  localStorage.removeItem('isLoggedin');
   }
 
   inputInvalid(value) {
     const formControl: AbstractControl = this.loginForm.controls[value];
-
     if (formControl.touched && formControl.errors) {
       return Object.keys(formControl.errors);
     }
-
     return [];
   }
 
 
   login() {
-
-    /*
-      console.log("Chamou o método login()");    
-      console.log("user: "+ this.loginForm.value.username);
-      console.log("password: "+ this.loginForm.value.password);
-    */ 
-      
-      this.loginService.loginHC(this.loginForm.value.username , this.loginForm.value.password).subscribe((usuario: Usuario) => {
-        localStorage.setItem('isLoggedin', usuario.token );
-      // console.log(usuario.token);
-        this.router.navigate(['dashboard/prontuarios/pacientes-internados']);
-      },
-      (err)=>{this.isInvalidLogin = true;console.log(err);}  
-    );    
+    this.loginService.loginHC(this.loginForm.value.username, this.loginForm.value.password).subscribe((usuario: Usuario) => {
+      localStorage.setItem('token', usuario.token);
+      localStorage.setItem('user', usuario.username);
+      this.router.navigate(['dashboard/prontuarios/pacientes-internados']);
+    },
+      (err) => {
+        this.isInvalidLogin = true;
+        console.log(err); }
+    );
   }
 }
